dd984e89195334bace293be8e4e0996c2f66c0e5
mudbile <eulerspill@protonmail.com>
2018-08-12 00:38:21 +1000

    previous button

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -11,6 +11,7 @@ public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mPreviousButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[] {
@@ -48,6 +49,20 @@ public class QuizActivity extends AppCompatActivity {
                 checkAnswer(false);
             }
         });
+
+        mPreviousButton = (Button) findViewById(R.id.previous_button);
+        mPreviousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int newIndex = mCurrentQuestionIndex - 1;
+                if (newIndex < 0){
+                    newIndex = mQuestionBank.length - 1;
+                }
+                mCurrentQuestionIndex = newIndex % mQuestionBank.length; //just in case
+                updateQuestion();
+            }
+        });
+
 
         mNextButton = (Button) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
